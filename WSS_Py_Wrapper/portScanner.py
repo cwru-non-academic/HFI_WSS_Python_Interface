2ee8b599c03fdb8de305038dc30e306b7f356b35
schramfield <shs120@case.edu>
2025-11-07 16:55:47 -0500

    Portscanner translated to py

--- WSS_Py_Wrapper/portScanner.py
+++ WSS_Py_Wrapper/portScanner.py
@@ -9,6 +9,12 @@ import serial
 from serial.tools.list_ports import comports
 
 print("Scanning for serial ports...")
+COM = "None Selected"
 for port in comports():
     print(port)
+print("Please select your COM port:")
+COM = input()
+COM = COM.upper()
+print(COM, "selected.\t Connecting...")
 
+ser = serial.Serial(COM)
